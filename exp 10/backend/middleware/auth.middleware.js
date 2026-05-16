02e80640836139798f75512e7edff91d4b80fa2f
Sri Rangan <srirangan0603@gmail.com>
2026-05-16 17:42:27 +0530

    Update auth.middleware.js

--- exp 10/backend/middleware/auth.middleware.js
+++ exp 10/backend/middleware/auth.middleware.js
@@ -5,10 +5,16 @@ module.exports = (req, res, next) => {
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
+
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
-  } catch {
+    
+  } 
+  
+  
+  catch {
     res.status(401).json({ message: "Invalid or expired token" });
   }
 };
