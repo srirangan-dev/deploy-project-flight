61ab4b7a0e71ef76a84e2ad78e1675c01a7473d9
Sri Rangan <srirangan0603@gmail.com>
2026-06-06 20:07:21 +0530

    Update main.jsx

--- exp 10/src/main.jsx
+++ exp 10/src/main.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Put it here
 import App from './App';
-
 import './index.css';
-
 import { BookingProvider } from './context/BookingContext';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
