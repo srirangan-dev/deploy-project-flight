336c5f7b97a9027f433b39fb0f75b44a27abcd92
Sri Rangan <srirangan0603@gmail.com>
2026-06-16 11:55:45 +0530

    Update App.jsx

--- exp 10/src/App.jsx
+++ exp 10/src/App.jsx
@@ -15,6 +15,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+
+      
       <main className="container">
         <Routes>
           <Route path="/" element={<Home />} />
