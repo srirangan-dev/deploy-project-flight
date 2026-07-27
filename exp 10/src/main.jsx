9cdf73e755fb079c3a5f96ff578a702664ab39fe
Sri Rangan <srirangan0603@gmail.com>
2026-07-27 23:38:36 +0530

    Update main.jsx

--- exp 10/src/main.jsx
+++ exp 10/src/main.jsx
@@ -6,8 +6,6 @@ import './index.css';
 
 
 import { BookingProvider } from './context/BookingContext';
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter> 
